566cfd4fd66366f7e76488629ce495f666a2fc66
Raj Karmakar <shubhashish0612@gmail.com>
2025-04-14 16:28:14 +0530

    Update PHP_Form_Validation.php
    Chang has made

--- PHP_Form_Validation.php
+++ PHP_Form_Validation.php
@@ -11,7 +11,7 @@
             }
             body {
                 font-family: Arial, sans-serif;
-                background-color: #f2f2f2f2;
+                background-color: #f2f2f2;
                 margin: 0;
                 padding: 20px;
             }
